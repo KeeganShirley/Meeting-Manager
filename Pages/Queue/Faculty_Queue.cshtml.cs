5517c6d5c3fde0b3433b6d8a07a8b68468f98f84
KeeganShirley <keeganshirley757@gmail.com>
2023-02-08 12:56:21 -0500

    Updated UI

---
+++ Pages/Queue/Faculty_Queue.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Meeting_Manager.Pages.Queue
+{
+    public class Faculty_QueueModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
