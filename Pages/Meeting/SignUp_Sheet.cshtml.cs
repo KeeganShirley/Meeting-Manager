41d503ba90f96199a6f40cef9b0cd7c8bff57548
KeeganShirley <keeganshirley757@gmail.com>
2023-02-08 15:04:07 -0500

    Added Sign Up Sheet

---
+++ Pages/Meeting/SignUp_Sheet.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Meeting_Manager.Pages.Meeting
+{
+    public class SignUp_SheetModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
