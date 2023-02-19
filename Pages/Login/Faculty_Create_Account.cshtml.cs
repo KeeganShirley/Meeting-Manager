19eba0e1d0b28c934ec5f97c9d0f914f504a72da
Keegan Shirley <shirlekw@dukes.jmu.edu>
2023-02-19 18:23:08 -0500

    jus some tweaks. Need to make it so when we create username and password it inserts the data into our credentials table.

--- Pages/Login/Faculty_Create_Account.cshtml.cs
+++ Pages/Login/Faculty_Create_Account.cshtml.cs
@@ -5,6 +5,11 @@ namespace Meeting_Manager.Pages.Login
 {
     public class Faculty_Create_AccountModel : PageModel
     {
+        [BindProperty]
+        public string Username { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+
         public void OnGet()
         {
         }
--- Pages/Login/Student_Create_Account.cshtml.cs
+++ Pages/Login/Student_Create_Account.cshtml.cs
@@ -5,8 +5,16 @@ namespace Meeting_Manager.Pages.Login
 {
     public class Student_Create_AccountModel : PageModel
     {
-        public void OnGet()
+		[BindProperty]
+		public string Username { get; set; }
+		[BindProperty]
+		public string Password { get; set; }
+
+		public void OnGet()
         {
         }
-    }
+
+		
+		
+	}
 }
